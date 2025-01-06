509afc8d92f28345760d4fda904fcaa5e89318e2
TimofeiKazakov <violet09502@gmail.com>
2025-01-06 19:01:10 +0300

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import requests
+
+# Pandas — это программная библиотека на языке Python для обработки и анализа данных.
+# Она предоставляет специальные структуры данных и операции для манипулирования числовыми
+# таблицами и временными рядами.
+
+Data = [1, 3, 2, 5, 6, 9, 4]
+s = pd.Series(Data)
+Index = ["a", "b", "c", "d", "e", "f", "g"]
+si = pd.Series(Data, Index)
+print(si)
+
+#NumPy — это библиотека для языка программирования Python, добавляющая поддержку больших многомерных массивов и матриц,
+#а также большой набор высокоуровневых математических функций для работы с этими массивами.
+
+print()
+# Создание двумерного массива
+A = np.array([[1, 2, 3], [4, 5, 6]])
+print("Двумерный массив:\n", A)
+# Сумма элементов двумерного массива
+print("Сумма элементов двумерного массива:\n", np.sum(A))
+
+#Requests — это модуль для языка Python, который используют для упрощения работы с HTTP-запросами.
+# Поиск местонахождения для запросов на GitHub
+response = requests.get(
+    'https://api.github.com/search/repositories',
+    params={'q': 'requests+language:python'},
+)
+print()
+# Анализ некоторых атрибутов местонахождения запросов
+json_response = response.json()
+repository = json_response['items'][0]
+print(f'Repository name: {repository["name"]}')  # Python 3.6+
+print(f'Repository description: {repository["description"]}')  # Python 3.6+
